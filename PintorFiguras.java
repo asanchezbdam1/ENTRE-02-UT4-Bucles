13f509f896a5e30a1676b17a44f3c98f4c2decc3
Asier Sánchez <asanchezbdam1@gmail.com>
2020-11-11 21:35:02 +0100

    PintorFiguras completado

--- PintorFiguras.java
+++ PintorFiguras.java
@@ -19,8 +19,25 @@ public class PintorFiguras
      * 
      * (usa bucles for)
      */
-    public  void dibujarFigura(int altura) {
-        
+    public void dibujarFigura(int altura) {
+        for (int i = 1; i <= altura; i++) {
+            int espacios = (altura - i) * ANCHO_BLOQUE / 2;
+            for (int j = 0; j < espacios; j++) {
+                System.out.print(ESPACIO);
+            }
+            
+            for (int j = 0; j < i; j++) {
+                for (int k = 0; k < ANCHO_BLOQUE; k++) {
+                    if (j % 2 == 0) {
+                        System.out.print(CAR1);
+                    }
+                    else {
+                        System.out.print(CAR2);
+                    }
+                }
+            }
+            System.out.println();
+        }
     }
 
     /**
